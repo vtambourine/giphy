56809286e54cc2ecd143a5f2f472e4bf1800fb5e
Benjamin T <vtambourine@ya.ru>
2019-06-25 01:27:53 +0200

    Use throttled submit function

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,33 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import Search from './Search';
 import Feed from './Feed';
 import { throttle } from 'lodash';
 
 import styles from './App.module.css';
 
-const req = require('./fixture/data').data as GIF[];
+function useProvier(initialState: GIF[]): [GIF[], (q: string) => void] {
+  const [state, setState] = useState<GIF[]>(initialState);
+  function submit(query: string) {
+    const data = require('./fixture/data').data as GIF[];
+    setState(data);
+  }
 
-const data = [{}, {}, {}, {}, {}] as GIF[];
-
-const submit = throttle((query: string) => console.log(query), 1000, {
-  leading: false,
-});
+  return [state, submit];
+}
 
 const App: React.FC = () => {
+  const [data, submit] = useProvier([] as GIF[]);
+  const throttledSubmit = useRef(throttle(submit, 1000, { leading: false }))
+    .current;
+
   return (
     <div className={styles.app}>
       <div className={styles.container}>
         <div className={styles.search}>
-          <Search onChange={submit} />
+          <Search onChange={q => throttledSubmit(q)} />
         </div>
         <div className={styles.feed}>
-          <Feed data={req} />
+          <Feed data={data} />
         </div>
       </div>
     </div>
