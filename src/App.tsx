656d448bec3351592d1b0804450879f124ee58ab
Benjamin T <vtambourine@ya.ru>
2019-06-25 14:00:18 +0200

    Implement endless scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,86 @@
-import React, { useState, useRef } from 'react';
 import { throttle } from 'lodash';
-import Search from './Search';
+import cx from 'classnames';
+import React, { useRef, useState } from 'react';
+
 import Feed from './Feed';
 import Provider from './provider';
-import cx from 'classnames';
+import Search from './Search';
 
 import styles from './App.module.css';
 
-function useProvier(initialState: GIF[]): [GIF[], (q: string) => void] {
-  const [state, setState] = useState<GIF[]>(initialState);
-  function submit(query: string) {
-    const provider = new Provider(query);
-    provider.next().then(response => setState(response.data));
-  }
+interface IAppProps {}
 
-  return [state, submit];
+interface IAppState {
+  isLoading: boolean;
+  data: GIF[];
 }
 
-const App: React.FC = () => {
-  const [data, submit] = useProvier([] as GIF[]);
-  const throttledSubmit = useRef(throttle(submit, 1500, { leading: false }))
-    .current;
+class App extends React.Component<IAppProps, IAppState> {
+  state = {
+    isLoading: false,
+    data: [] as GIF[],
+  };
 
-  return (
-    <div className={styles.app}>
-      <div
-        className={cx(styles.container, {
-          [styles.empty]: data.length === 0,
-        })}
-      >
-        <div className={styles.search}>
-          <Search onChange={q => throttledSubmit(q)} />
-        </div>
-        <div className={styles.feed}>
-          <Feed data={data} />
+  throttledSubmit: (query: string) => void;
+  provider: Provider;
+
+  constructor(props: IAppProps) {
+    super(props);
+    this.provider = new Provider('', {
+      limit: 20,
+    });
+    this.throttledSubmit = throttle(this.onSubmit, 1500, { leading: false });
+  }
+
+  onSubmit = (query: string) => {
+    this.setState({
+      isLoading: true,
+    });
+    this.provider = new Provider(query, {
+      limit: 20,
+    });
+    this.provider.next().then(({ data }) =>
+      this.setState({
+        isLoading: false,
+        data,
+      }),
+    );
+  };
+
+  onFeedEnd = () => {
+    if (!this.state.isLoading) {
+      this.setState({
+        isLoading: true,
+      });
+
+      this.provider.next().then(({ data }) =>
+        this.setState({
+          isLoading: false,
+          data: [...this.state.data, ...data],
+        }),
+      );
+    }
+  };
+
+  render() {
+    const { data } = this.state;
+    return (
+      <div className={styles.app}>
+        <div
+          className={cx(styles.container, {
+            [styles.empty]: data.length === 0,
+          })}
+        >
+          <div className={styles.search}>
+            <Search onChange={this.throttledSubmit} />
+          </div>
+          <div className={styles.feed}>
+            <Feed data={data} onFeedEnd={this.onFeedEnd} />
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
--- src/Feed.tsx
+++ src/Feed.tsx
@@ -1,23 +1,55 @@
-import React from 'react';
+import { throttle } from 'lodash';
+import React, { useEffect, useRef } from 'react';
+
 import styles from './Feed.module.css';
+
+const THROTTLE_TIMEOUT = 150;
+const OVERLAP_GAP = 320;
 
 interface IFeedProps {
   data: GIF[];
+  onFeedEnd: () => void;
 }
 
 const Feed: React.FC<IFeedProps> = props => {
+  const scrollElement = useRef<HTMLDivElement>(null);
+
+  const fistGIF = props.data[0];
+  useEffect(() => {
+    if (scrollElement && scrollElement.current) {
+      scrollElement.current.scrollTo({ top: 0 });
+    }
+  }, [fistGIF]);
+
+  useEffect(() => {
+    function handleScroll() {
+      if (scrollElement && scrollElement.current) {
+        const { clientHeight, scrollTop, scrollHeight } = scrollElement.current;
+        if (scrollHeight - (scrollTop + clientHeight) < OVERLAP_GAP) {
+          props.onFeedEnd();
+        }
+      }
+    }
+
+    const throttledHandleScroll = throttle(handleScroll, THROTTLE_TIMEOUT);
+
+    const currentScrollElement = scrollElement.current;
+    if (scrollElement && scrollElement.current) {
+      scrollElement.current.addEventListener('scroll', throttledHandleScroll);
+    }
+
+    return () => {
+      if (currentScrollElement) {
+        currentScrollElement.removeEventListener(
+          'scroll',
+          throttledHandleScroll,
+        );
+      }
+    };
+  });
+
   return (
-    <div className={styles.feed}>
-      {props.data.map((gif, key) => (
-        <div className={styles.image} key={key}>
-          <img
-            src={gif.images.fixed_width_still.url}
-            alt={gif.title}
-            height={gif.images.fixed_width_still.height}
-            width={gif.images.fixed_width_still.width}
-          />
-        </div>
-      ))}
+    <div className={styles.feed} ref={scrollElement}>
       {props.data.map((gif, key) => (
         <div className={styles.image} key={key}>
           <img
